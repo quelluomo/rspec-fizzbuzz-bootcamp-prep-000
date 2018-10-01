faaaac3a0ae633a0d57b9e320a2669062533e25e
Matteo Muscas <thatman.j@gmail.com>
2018-10-01 12:18:08 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,15 +3,15 @@
 
 def fizzbuzz(int)
   if int % 3 == 0
-    return "Fizz"
+    "Fizz"
   end
   
   if int % 5 == 0
-    return "Buzz"
+    "Buzz"
   end
   
-  elsif int % 3 = 0 && int % 5 == 0
-    return "FizzBuzz"
+  if int % 3 = 0 && int % 5 == 0
+    "FizzBuzz"
   end
   
 end
